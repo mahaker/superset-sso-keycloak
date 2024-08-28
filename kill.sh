4f3d4cb0af31dc8b200b737dcce562cda4dfebb6
Hideaki Matsunami <carbon0409@gmail.com>
2024-08-28 18:55:26 +0900

    add keycloak container

--- kill.sh
+++ kill.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$(cd $(dirname $0) && pwd)
-cd $SCRIPT_DIR
-
 docker stop my_postgres
 docker container rm my_postgres
 docker image rm my_postgres
+
+docker stop my_keycloak
+docker container rm my_keycloak
+docker image rm my_keycloak
--- run.sh
+++ run.sh
@@ -6,3 +6,11 @@ cd $SCRIPT_DIR
 cd postgres
 docker build -t my_postgres .
 docker run --name my_postgres -p 5435:5432 --env-file ../env.list -d my_postgres
+
+cd ../keycloak
+docker build -t my_keycloak .
+# TODO production settings
+# TODO enable /health endpoint(currently, 'Resource not found')
+# memo: To connects my_postgres, set --add-host option
+# https://stackoverflow.com/questions/24319662/from-inside-of-a-docker-container-how-do-i-connect-to-the-localhost-of-the-mach
+docker run --name my_keycloak -p 8080:8080 -p 9000:9000 --env-file ../env.list --add-host=host.docker.internal:host-gateway -d my_keycloak start-dev
