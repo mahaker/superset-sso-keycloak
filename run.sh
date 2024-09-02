b53285854e2aa574b403fc17912e45afc4d30ee1
Hideaki Matsunami <carbon0409@gmail.com>
2024-09-02 19:53:53 +0900

    Add Keycloak custom theme

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ docker build -t my_keycloak .
 # TODO enable /health endpoint(currently, 'Resource not found')
 # memo: To connects my_postgres, set --add-host option
 # https://stackoverflow.com/questions/24319662/from-inside-of-a-docker-container-how-do-i-connect-to-the-localhost-of-the-mach
-docker run --name my_keycloak -p 8080:8080 -p 9000:9000 --env-file ../env.list --add-host=host.docker.internal:host-gateway -d my_keycloak start-dev
+docker run --name my_keycloak -p 8080:8080 -p 9000:9000 --env-file ../env.list --add-host=host.docker.internal:host-gateway -d my_keycloak start-dev  --spi-theme-static-max-age=-1 --spi-theme-cache-themes=false --spi-theme-cache-templates=false
 
 cd ../superset
 docker compose -f docker-compose-non-dev.yml up -d
